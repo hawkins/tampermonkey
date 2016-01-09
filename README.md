12a5d298bc5ffd29fad50adc326130cb449f64c3
Josh Hawkins <jkhaccounts@gmail.com>
2016-01-08 22:21:09 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+tampermonkey
+============
+
+This repository has my personal tampermonkey scripts. They're not concerned with being brilliant or cross-system, as they're typically just scripts I throw together in a quick 5-minute session to fix small pet peeves with web pages on my desktop. Regardless, here's how to use them:
+
+## Usage
+
+* Each folder is a reference to where the script is helpful. I.e, "initium" folder is used for scripts for http://playinitium.com/ to modify css on the in-game chat.
+* Simply follow the instructions outlined [here](http://stackoverflow.com/questions/5258989/manually-adding-a-userscript-to-google-chrome) to install them.
---
+++ initium/Chat Modifications.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Chat Modifications
+// @namespace    http://tampermonkey.net/
+// @version      0.4
+// @description  Shortens names and some more
+// @author       Hawkins
+// @match        https://www.playinitium.com/*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+
+//var i = setTimeout(function() {
+$(document).ready( function() {
+    setTimeout(function() {
+        // Shorten those infamous long names
+        $("a.premium-character-name:contains(' Erl ')").html("Erl")
+        $("a.premium-character-name:contains('Diagun')").html("Diagun")
+        $("a.clue:contains('SPOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOKS')").html("Spooks")
+
+        // Add CSS to message body of dev messages
+        $("a.content-dev-nickname").parent().siblings().addClass( "content-dev-nickname" );
+
+        // Add CSS to bot messages
+        $("a:contains('[Bot]')").addClass( "content-dev-nickname" );
+        $("a:contains('[Bot]')").css( "font-style", "italic" );
+        /*$("a:contains('[Bot]')").css( "background", "indigo" );
+        $("a:contains('[Bot]')").css( "-webkit-background-clip", "text" ); 
+        $("a:contains('[Bot]')").css( "-webkit-text-fill-color",  "transparent" );*/
+        $("a:contains('[Bot]')").parent().siblings().css( "background", "violet" );
+        $("a:contains('[Bot]')").parent().siblings().css( "-webkit-background-clip", "text" ); 
+        $("a:contains('[Bot]')").parent().siblings().css( "-webkit-text-fill-color",  "transparent" );
+        $("a:contains('[Bot]')").parent().siblings().css( "font-style", "italic" );
+    }, 300);
+});
